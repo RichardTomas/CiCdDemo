6fff4e4ce6422d35497bf89a7577f0f445795880
allantomas <32474234+RichardTomas@users.noreply.github.com>
2022-11-02 19:09:18 -0700

    updated test

--- hello-circle/src/test/java/com/example/hellocircle/controller/HelloCircleControllerTest.java
+++ hello-circle/src/test/java/com/example/hellocircle/controller/HelloCircleControllerTest.java
@@ -1,7 +1,10 @@
 package com.example.hellocircle.controller;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.Assert.*;
@@ -9,7 +12,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+@RunWith(SpringRunner.class)
+@WebMvcTest(HelloCircleController.class)
 public class HelloCircleControllerTest {
     @Autowired
     private MockMvc mockMvc;
